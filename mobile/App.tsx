ec468a24c7aad263e5050112828dbfead5295cfe
rafael <rafael.santana@webjump.com.br>
2023-05-22 16:29:51 -0300

    feat: create projects

---
+++ mobile/App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-5xl font-bold text-gray-50">
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="light" translucent />
+    </View>
+  )
+}
---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import { fastify } from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify({ logger: true })
+const prisma = new PrismaClient()
+
+app.get('/users', async (request, reply) => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then((address) => {
+    console.log(`Server is listening on ${address}`)
+  })
